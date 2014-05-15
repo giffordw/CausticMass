143f2fbeae07f89bd11ac23ee699ec02bcecf3b6
Dan Gifford <dgifford19@gmail.com>
2014-05-14 21:54:13 -0400

    initial setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='causticpy',
+      version='0.3',
+      description='Caustic mass estimator for astrophysical systems',
+      url='http://github.com/giffordw/causticpy',
+      author='Dan Gifford',
+      author_email='',
+      license='',
+      packages=['causticpy'],
+      zip_safe=False)
+
